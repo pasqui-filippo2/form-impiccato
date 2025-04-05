e230e794f852c9997ff3000c72d5757c89dd9fef
filippo <pasqui.filippo@franchettisalviani.net>
2025-04-05 09:12:09 +0200

    1-version

---
+++ impiccato-form/Form1.Designer.cs
@@ -0,0 +1,519 @@
+﻿namespace impiccato_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            button19 = new Button();
+            button20 = new Button();
+            button21 = new Button();
+            button22 = new Button();
+            button23 = new Button();
+            textBox1 = new TextBox();
+            panel1 = new Panel();
+            label5 = new Label();
+            nup1 = new NumericUpDown();
+            label1 = new Label();
+            label2 = new Label();
+            btnEasy = new Button();
+            btnMedium = new Button();
+            btnHard = new Button();
+            label3 = new Label();
+            txtParola = new TextBox();
+            btnGenerete = new Button();
+            panel2 = new Panel();
+            label4 = new Label();
+            pictureBox1 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)nup1).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(217, 273);
+            button1.Name = "button1";
+            button1.Size = new Size(33, 29);
+            button1.TabIndex = 0;
+            button1.Text = "A";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(451, 273);
+            button2.Name = "button2";
+            button2.Size = new Size(33, 29);
+            button2.TabIndex = 1;
+            button2.Text = "G";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(412, 273);
+            button3.Name = "button3";
+            button3.Size = new Size(33, 29);
+            button3.TabIndex = 2;
+            button3.Text = "F";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(373, 273);
+            button4.Name = "button4";
+            button4.Size = new Size(33, 29);
+            button4.TabIndex = 3;
+            button4.Text = "E";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(334, 273);
+            button5.Name = "button5";
+            button5.Size = new Size(33, 29);
+            button5.TabIndex = 4;
+            button5.Text = "D";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(295, 273);
+            button6.Name = "button6";
+            button6.Size = new Size(33, 29);
+            button6.TabIndex = 5;
+            button6.Text = "C";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(256, 273);
+            button7.Name = "button7";
+            button7.Size = new Size(33, 29);
+            button7.TabIndex = 6;
+            button7.Text = "B";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(256, 343);
+            button8.Name = "button8";
+            button8.Size = new Size(33, 29);
+            button8.TabIndex = 13;
+            button8.Text = "R";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(295, 343);
+            button9.Name = "button9";
+            button9.Size = new Size(33, 29);
+            button9.TabIndex = 12;
+            button9.Text = "S";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(334, 343);
+            button10.Name = "button10";
+            button10.Size = new Size(33, 29);
+            button10.TabIndex = 11;
+            button10.Text = "T";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(373, 343);
+            button11.Name = "button11";
+            button11.Size = new Size(33, 29);
+            button11.TabIndex = 10;
+            button11.Text = "U";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(412, 343);
+            button12.Name = "button12";
+            button12.Size = new Size(33, 29);
+            button12.TabIndex = 9;
+            button12.Text = "V";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(451, 343);
+            button13.Name = "button13";
+            button13.Size = new Size(33, 29);
+            button13.TabIndex = 8;
+            button13.Text = "Z";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(217, 343);
+            button14.Name = "button14";
+            button14.Size = new Size(33, 29);
+            button14.TabIndex = 7;
+            button14.Text = "Q";
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            button15.Location = new Point(256, 308);
+            button15.Name = "button15";
+            button15.Size = new Size(33, 29);
+            button15.TabIndex = 20;
+            button15.Text = "I";
+            button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            button16.Location = new Point(295, 308);
+            button16.Name = "button16";
+            button16.Size = new Size(33, 29);
+            button16.TabIndex = 19;
+            button16.Text = "L";
+            button16.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            button17.Location = new Point(334, 308);
+            button17.Name = "button17";
+            button17.Size = new Size(33, 29);
+            button17.TabIndex = 18;
+            button17.Text = "M";
+            button17.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            button18.Location = new Point(373, 308);
+            button18.Name = "button18";
+            button18.Size = new Size(33, 29);
+            button18.TabIndex = 17;
+            button18.Text = "N";
+            button18.UseVisualStyleBackColor = true;
+            // 
+            // button19
+            // 
+            button19.Location = new Point(412, 308);
+            button19.Name = "button19";
+            button19.Size = new Size(33, 29);
+            button19.TabIndex = 16;
+            button19.Text = "O";
+            button19.UseVisualStyleBackColor = true;
+            // 
+            // button20
+            // 
+            button20.Location = new Point(451, 308);
+            button20.Name = "button20";
+            button20.Size = new Size(33, 29);
+            button20.TabIndex = 15;
+            button20.Text = "P";
+            button20.UseVisualStyleBackColor = true;
+            // 
+            // button21
+            // 
+            button21.Location = new Point(217, 308);
+            button21.Name = "button21";
+            button21.Size = new Size(33, 29);
+            button21.TabIndex = 14;
+            button21.Text = "H";
+            button21.UseVisualStyleBackColor = true;
+            // 
+            // button22
+            // 
+            button22.Location = new Point(490, 273);
+            button22.Name = "button22";
+            button22.Size = new Size(51, 99);
+            button22.TabIndex = 21;
+            button22.Text = "CANC";
+            button22.UseVisualStyleBackColor = true;
+            // 
+            // button23
+            // 
+            button23.Location = new Point(217, 232);
+            button23.Name = "button23";
+            button23.Size = new Size(135, 25);
+            button23.TabIndex = 22;
+            button23.Text = "INVIA PAROLA";
+            button23.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(358, 232);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(183, 23);
+            textBox1.TabIndex = 23;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveBorder;
+            panel1.BorderStyle = BorderStyle.Fixed3D;
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(nup1);
+            panel1.Location = new Point(616, 95);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(183, 86);
+            panel1.TabIndex = 24;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(77, 21);
+            label5.Name = "label5";
+            label5.Size = new Size(38, 15);
+            label5.TabIndex = 1;
+            label5.Text = "label5";
+            // 
+            // nup1
+            // 
+            nup1.Enabled = false;
+            nup1.Location = new Point(30, 46);
+            nup1.Name = "nup1";
+            nup1.ReadOnly = true;
+            nup1.Size = new Size(120, 23);
+            nup1.TabIndex = 0;
+            nup1.TextAlign = HorizontalAlignment.Center;
+            nup1.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Viner Hand ITC", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(217, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(339, 52);
+            label1.TabIndex = 25;
+            label1.Text = "Gioco Dell'impiccato";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(345, 58);
+            label2.Name = "label2";
+            label2.Size = new Size(71, 15);
+            label2.TabIndex = 26;
+            label2.Text = "DIFFICOLTA'";
+            // 
+            // btnEasy
+            // 
+            btnEasy.Location = new Point(235, 78);
+            btnEasy.Name = "btnEasy";
+            btnEasy.Size = new Size(75, 23);
+            btnEasy.TabIndex = 27;
+            btnEasy.Text = "FACILE";
+            btnEasy.UseVisualStyleBackColor = true;
+            btnEasy.Click += btnEasy_Click;
+            // 
+            // btnMedium
+            // 
+            btnMedium.Location = new Point(326, 78);
+            btnMedium.Name = "btnMedium";
+            btnMedium.Size = new Size(75, 23);
+            btnMedium.TabIndex = 28;
+            btnMedium.Text = "MEDIO";
+            btnMedium.UseVisualStyleBackColor = true;
+            btnMedium.Click += btnMedium_Click;
+            // 
+            // btnHard
+            // 
+            btnHard.Location = new Point(423, 78);
+            btnHard.Name = "btnHard";
+            btnHard.Size = new Size(75, 23);
+            btnHard.TabIndex = 29;
+            btnHard.Text = "DIFFICILE";
+            btnHard.UseVisualStyleBackColor = true;
+            btnHard.Click += btnHard_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(154, 403);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 15);
+            label3.TabIndex = 30;
+            // 
+            // txtParola
+            // 
+            txtParola.Location = new Point(358, 159);
+            txtParola.Name = "txtParola";
+            txtParola.ReadOnly = true;
+            txtParola.Size = new Size(183, 23);
+            txtParola.TabIndex = 32;
+            // 
+            // btnGenerete
+            // 
+            btnGenerete.Location = new Point(217, 158);
+            btnGenerete.Name = "btnGenerete";
+            btnGenerete.Size = new Size(135, 23);
+            btnGenerete.TabIndex = 33;
+            btnGenerete.Text = "GENERA PAROLA -->";
+            btnGenerete.UseVisualStyleBackColor = true;
+            btnGenerete.Click += btnGenerete_Click;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ActiveBorder;
+            panel2.BorderStyle = BorderStyle.Fixed3D;
+            panel2.Controls.Add(label4);
+            panel2.Location = new Point(217, 394);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(324, 35);
+            panel2.TabIndex = 31;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(16, 8);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 0;
+            label4.Text = "label4";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(153, 320);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 34;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(btnGenerete);
+            Controls.Add(txtParola);
+            Controls.Add(panel2);
+            Controls.Add(label3);
+            Controls.Add(btnHard);
+            Controls.Add(btnMedium);
+            Controls.Add(btnEasy);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            Controls.Add(textBox1);
+            Controls.Add(button23);
+            Controls.Add(button22);
+            Controls.Add(button15);
+            Controls.Add(button16);
+            Controls.Add(button17);
+            Controls.Add(button18);
+            Controls.Add(button19);
+            Controls.Add(button20);
+            Controls.Add(button21);
+            Controls.Add(button8);
+            Controls.Add(button9);
+            Controls.Add(button10);
+            Controls.Add(button11);
+            Controls.Add(button12);
+            Controls.Add(button13);
+            Controls.Add(button14);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)nup1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button18;
+        private Button button19;
+        private Button button20;
+        private Button button21;
+        private Button button22;
+        private Button button23;
+        private TextBox textBox1;
+        private Panel panel1;
+        private Label label1;
+        private Label label2;
+        private Button btnEasy;
+        private Button btnMedium;
+        private Button btnHard;
+        private Label label3;
+        private TextBox txtParola;
+        private Label label5;
+        private NumericUpDown nup1;
+        private Button btnGenerete;
+        private Panel panel2;
+        private Label label4;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ impiccato-form/Form1.cs
@@ -0,0 +1,90 @@
+namespace impiccato_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int tentativi = 0;
+        string Psecret = "";
+        char[] lettere = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'z' };
+        bool easy = false, medium = false, hard = false;
+        Random rnd = new Random();
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEasy_Click(object sender, EventArgs e)
+        {
+            tentativi = 7;
+            easy = true;
+            nup1.Value = tentativi;
+        }
+
+        private void btnMedium_Click(object sender, EventArgs e)
+        {
+            tentativi = 6;
+            nup1.Value = tentativi;
+        }
+
+        private void btnHard_Click(object sender, EventArgs e)
+        {
+            tentativi = 5;
+            nup1.Value = tentativi;
+        }
+
+        private void btnGenerete_Click(object sender, EventArgs e)
+        {
+            string filePath = "1st-level.txt.txt"; // Nome del file CSV 
+
+            string[] lines = File.ReadAllLines(filePath); // Legge tutte le righe e le mette in un vettore
+            int random = rnd.Next(1, lines.Length - 1);
+            Psecret = lines[random];
+            char[] secret = new char[Psecret.Length];
+            if (easy == true)
+            {
+                for (int i = 0; i < Psecret.Length; i++)
+                {
+                    for (int j = 0; j < lettere.Length; j++)
+                    {
+
+                        if (Psecret[i] == lettere[j])
+                        {
+                            if (i == 0)
+                            {
+                                secret[i] = lettere[j];
+
+                            }
+                            else if (i == random)
+                            {
+                                secret[i] = lettere[j];
+                            }
+                            else
+                            {
+                                secret[i] = '_';
+                            }
+
+                        }
+
+
+                    }
+                }
+               
+            }
+            
+            
+        }
+    }
+}
